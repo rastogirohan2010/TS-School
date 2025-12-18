94fc85d231cd3ffcff1915e0f5f33dd1d4aab009
rastogirohan2010 <rastogirohan2010@gmail.com>
2025-12-18 11:02:14 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.TS_CONFIG = {
+  firebase: {
+    apiKey: "PASTE_YOURS",
+    authDomain: "PASTE_YOURS",
+    projectId: "PASTE_YOURS",
+  }
+  // Later (optional):
+  // geminiApiKey: "PASTE_YOURS"
+};
